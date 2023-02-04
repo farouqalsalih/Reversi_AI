35968ce1e7fafc7a77b94f747a73a2b43229c82e
ddawg1000 <R3ddev1l81@gmail.com>
2023-02-04 12:18:06 -0500

    initial commit, added ReversiGame.java

---
+++ ReversiGame.java
@@ -0,0 +1,10 @@
+public class ReversiGame {
+    
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Reversi by Daniel Nadj and Farouq Alsalih\n");
+        System.out.println("Choose your game:\n\t1. Small 4x4 Reversi\n\t2. Standard 8x8 Reversi");
+    }
+}
